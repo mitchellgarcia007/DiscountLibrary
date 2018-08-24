9893a6aa660cde4ddc730c530bc8782111ce438c
mitchellgarcia007 <ca103mitchellgarcia@yahoo.com>
2018-08-23 22:34:10 -0700

    add page and other

---
+++ add/index.php
@@ -0,0 +1,146 @@
+<?php
+    include $_SERVER['DOCUMENT_ROOT']."/connection.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>USI Discounts Library</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel='stylesheet' type='text/css' href='http://code.jquery.com/ui/1.9.2/themes/base/jquery-ui.css'/>
+<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js"></script>
+</head>
+<body style="overflow-x:hidden;margin-bottom:400px">
+
+<nav class="navbar navbar-inverse" style="border-radius:0px"> 
+  <div class="container-fluid">
+    <div class="navbar-header" style="width:100%">
+        <a class="navbar-brand" href="/">USI Discounts Library</a>
+    </div>
+  </div>
+</nav>
+  
+<div class="container">
+  <h3 class="text-center"> Add Discount </h3>
+
+    <div id="divForm">
+
+        <form action="#">
+            <div class="form-group">
+                <label for="">Company Name:</label>
+                <input type="text" class="form-control" id="companyName" required>
+            </div>
+            <div class="form-group">
+                <label for="">Promotion Name:</label>
+                <input type="text" class="form-control" id="promotionName" required>
+            </div>
+            <div class="form-group">
+                <label for="">Promotion Description:</label>
+                <input type="text" class="form-control" id="promotionDescription" required>
+            </div>
+            <div class="form-group">
+                <label for="">Promotion Start Date:</label>
+                <input type="text" class="form-control" placeholder="Click here to select the Start Date" name="startDate" id="datepickerStartDate" readonly required>
+            </div>
+            <div class="form-group">
+                <label for="">Promotion Start Time:</label><br>
+                <select class="form-control" name="startDateHour" style="width:80px;display:inline" required>
+                    <option value="">Hour</option>
+                    <?php 
+                        for ($startTime = 1; $startTime <= 12; $startTime++) {
+                            echo "<option value='$startTime'>$startTime</option>";
+                        } 
+                    ?>
+                </select>
+                <span><strong>:</strong></span>
+                <select class="form-control" name="startDateMinute" style="width:90px;display:inline" required>
+                    <option value="">Minute</option>
+                    <option value="00">00</option>
+                    <option value="30">30</option>
+                </select>
+                <select class="form-control" name="startDateAMPM" style="width:95px;display:inline" required>
+                    <option value="">AM/PM</option>
+                    <option value="AM">AM</option>
+                    <option value="PM">PM</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="">Promotion End Date:</label>
+                <input type="text" class="form-control" placeholder="Click here to select the End Date" name="endDate" id="datepickerEndtDate" readonly required>
+            </div>
+            <div class="form-group">
+                <label for="">Promotion End Time:</label><br>
+                <select class="form-control" name="endDateHour" style="width:80px;display:inline" required>
+                    <option value="">Hour</option>
+                    <?php 
+                        for ($startTime = 1; $startTime <= 12; $startTime++) {
+                            echo "<option value='$startTime'>$startTime</option>";
+                        } 
+                    ?>
+                </select>
+                <span><strong>:</strong></span>
+                <select class="form-control" name="endDateMinute" style="width:90px;display:inline" required>
+                    <option value="">Minute</option>
+                    <option value="00">00</option>
+                    <option value="30">30</option>
+                </select>
+                <select class="form-control" name="endDateAMPM" style="width:95px;display:inline" required>
+                    <option value="">AM/PM</option>
+                    <option value="AM">AM</option>
+                    <option value="PM">PM</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="">Industry:</label>
+                <input type="text" class="form-control" id="industry" required>
+            </div>
+
+            <button type="submit" class="btn btn-success">Submit</button>
+        </form>
+
+    </div>
+
+</div>
+
+<style>
+#divForm{
+    margin: 50px auto;
+    max-width: 700px;
+    height: auto;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    border-radius: 3px;
+    padding: 60px 30px 60px 30px;
+}
+</style>
+
+<script>
+$(document).ready(function(){
+    $("#datepickerStartDate, #datepickerEndtDate").datepicker({
+        minDate: 0,
+        dateFormat: "m/d/yy",
+        buttonText: "Select date",
+    });    
+
+    $("form").submit(function(e){
+        if(!$("#datepickerStartDate").val()){
+            e.preventDefault();
+            alert("Please select a start date.");
+            return;
+        }
+        if(!$("#datepickerEndtDate").val()){
+            e.preventDefault();
+            alert("Please select an end date.");
+            return;
+        }
+
+        $( "#createEventForm" ).submit();
+    });
+});
+</script>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -12,35 +12,42 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
-<body>
+<body style="overflow-x:hidden;margin-bottom:400px">
 
 <nav class="navbar navbar-inverse" style="border-radius:0px"> 
   <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">USI Discounts Library</a>
+    <div class="navbar-header" style="width:100%">
+        <a class="navbar-brand" href="/">USI Discounts Library</a>
+        <a class="navbar-brand" href="/add" style="float:right;color:white">Add</a>
     </div>
   </div>
 </nav>
   
 <div class="container">
-  <h3 class="text-center">Discounts</h3>
+  <h3 class="text-center"> Discounts </h3>
 
     <div class="form-group" style="width:200px;float:right;margin-bottom:50px">
       <label for="sel1">Industry</label>
       <select class="form-control" id="industrySelect" onchange="industrySelected(this.value)">
         <option value="">All</option>
-        <option value="home">Home</option>
-        <option value="electronics">Electronics</option>
+        <?php
+            $sql2 = " SELECT DISTINCT industry FROM discountsInfo ORDER BY industry ASC ";
+            $result2 = mysqli_query($conn, $sql2);
+            while($row = mysqli_fetch_assoc($result2)){
+                $industry = $row["industry"];
+                echo "<option value='$industry'>$industry</option>";
+            }
+        ?>
       </select>
     </div>
 
-    <table class="table table-hover" style="margin-top:50px" id="myTable">
-        <thead>
+    <table class="table table-hover table-responsive" style="margin-top:50px" id="myTable">
+        <thead class="hidden">
         <tr>
             <th class="text-center">Image</th>
             <th>Promotion Info.</th>
             <th>Description</th>
-            <th>Industry</th>
+            <th class="hidden">Industry</th>
         </tr>
         </thead>
         <tbody>
@@ -62,15 +69,15 @@
 
                 echo "<tr>";
                     echo "<td class='text-center'><img src='img/$image' style='max-width:200px'></td>";
-                    echo "<td style='vertical-align: middle;'>";
-                        echo "<p><strong>Company</strong> $companyName</p>";
-                        echo "<p><strong>Promotion</strong> $discountName</p>";
-                        echo "<p><strong>Starts</strong> $startDateTime_formatted</p>";
-                        echo "<p><strong>Ends</strong> $endDateTime_formatted</p>";
-                        echo "<p><strong>Industry</strong> $industry</p>";
+                    echo "<td>";
+                        echo "<p><strong>Company:</strong> $companyName</p>";
+                        echo "<p><strong>Promotion:</strong> $discountName</p>";
+                        echo "<p><strong>Starts:</strong> $startDateTime_formatted</p>";
+                        echo "<p><strong>Ends:</strong> $endDateTime_formatted</p>";
+                        echo "<p><strong>Industry:</strong> $industry</p>";
                     echo "</td>";
-                    echo "<td style='vertical-align: middle;'>$discountDescription</td>";
-                    echo "<td>$industry</td>";
+                    echo "<td><p><strong>Description:</strong></p>$discountDescription</td>";
+                    echo "<td class='hidden'>$industry</td>";
                 echo "</tr>";
             }
         ?>
@@ -81,12 +88,12 @@
 </div>
 
  <script>
-    function industrySelected(str){
-        var value = str.toLowerCase();
-        $("#myTable tr").filter(function() {
+function industrySelected(str){
+    var value = str.toLowerCase();
+    $("#myTable tr").filter(function() {
         $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-        });
-    }
+    });
+}
 </script> 
 
 </body>
